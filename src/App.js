d16e7e8087de6b50386016c9515cbbe4703cf072
Sk Tamim <sktamim225@gmail.com>
2022-09-29 16:13:27 +0530

    Completed Hero section. Changed some styles of navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import MainButton from "./components/button/MainButton";
 import Navbar from "./layout/navbar/Navbar";
+import Hero from "./pages/home/Hero";
 
 function App() {
 	return (
 		<>
 			<Navbar />
+			<Hero />
 		</>
 	);
 }
--- src/components/button/MainButton.js
+++ src/components/button/MainButton.js
@@ -4,18 +4,38 @@ import classes from "./MainButton.module.css";
 const MainButton = (props) => {
 	return props.href ? (
 		props.lg ? (
-			<a href={props.href} className={classes.mainBtnLg}>
+			<a
+				href={props.href}
+				className={`${classes.mainBtnLg} ${
+					props.className ? props.className : ""
+				}`}
+			>
 				{props.children}
 			</a>
 		) : (
-			<a href={props.href} className={classes.mainBtn}>
+			<a
+				href={props.href}
+				className={`${classes.mainBtn} ${
+					props.className ? props.className : ""
+				}`}
+			>
 				{props.children}
 			</a>
 		)
 	) : props.lg ? (
-		<button className={classes.mainBtnLg}>{props.children}</button>
+		<button
+			className={`${classes.mainBtnLg} ${
+				props.className ? props.className : ""
+			}`}
+		>
+			{props.children}
+		</button>
 	) : (
-		<button className={classes.mainBtn}>{props.children}</button>
+		<button
+			className={`${classes.mainBtn} ${props.className ? props.className : ""}`}
+		>
+			{props.children}
+		</button>
 	);
 };
 
